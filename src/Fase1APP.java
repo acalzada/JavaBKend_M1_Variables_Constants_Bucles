1976f1afff833ee3a5b6f980e192eb569d747d1c
angel <angel@mowoot.com>
2020-09-22 14:48:40 +0200

    Correccio problema Eclipse i pujada de fitxers de projecte eclipse.

--- src/Fase1APP.java
+++ src/Fase1APP.java
@@ -2,6 +2,7 @@
 public class Fase1APP {
 
 	public static void main(String[] args) {
+		
 		String nom = "Angel", cognom1 ="Calzada", cognom2 = "Defez";
 		int dia = 28, mes = 10, anyNaixement = 1982;
 		
